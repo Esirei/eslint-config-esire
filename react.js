d4765236c297bf890caa584dcd308840c9d06ea2
Esi're <esiri92@gmail.com>
2022-02-18 15:01:32 +0100

    Update react rules.

--- react.js
+++ react.js
@@ -6,4 +6,18 @@ module.exports = {
     "plugin:jsx-a11y/recommended",
     "./tailwindcss.js",
   ],
+
+  rules: {
+    "react-hooks/exhaustive-deps": 2,
+  },
+
+  overrides: [
+    {
+      files: ["*.{ts,tsx}"],
+
+      rules: {
+        "react/prop-types": 0,
+      },
+    },
+  ],
 };
